fa39fc6b50159ef83952556467388e255577052b
popkorn96 <mpaloma2014@gmail.com>
2020-09-14 20:33:37 -0500

    fixed migrations

--- sinatra_bills/db/migrate/20200914192702_create_bills.rb
+++ sinatra_bills/db/migrate/20200914192702_create_bills.rb
@@ -1,4 +1,10 @@
 class CreateBills < ActiveRecord::Migration
   def change
+    create_table :bills do |t|
+      t.string :bill_name
+      t.decimal :remaining_balance
+      t.decimal :amount_due
+      t.date :due_date
+    end
   end
 end
